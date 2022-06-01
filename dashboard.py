095c561a3b69ad2c46531abda483c332f23da9f5
pnadelofficial <peter.m.nadel@gmail.com>
2022-06-01 09:21:33 -0400

    list logic update

--- dashboard.py
+++ dashboard.py
@@ -14,7 +14,15 @@ if geo_select == geojson_list[0]:
   poly = openJSON(geojson_list[0])
   fig = plotlyPlot(poly)
   st.plotly_chart(fig, use_container_width=True)
-else:
+elif: geo_select == geojson_list[1]:
   poly = openJSON(geojson_list[1])
+  fig = plotlyPlot(poly)
+  st.plotly_chart(fig, use_container_width=True)
+elif: geo_select == geojson_list[2]:
+  poly = openJSON(geojson_list[2])
+  fig = plotlyPlot(poly)
+  st.plotly_chart(fig, use_container_width=True)
+else:
+  poly = openJSON(geojson_list[3])
   fig = plotlyPlot(poly)
   st.plotly_chart(fig, use_container_width=True)
